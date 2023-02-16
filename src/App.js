60b15769bafb8400e67cfbf594442468044e5162
Jose Costa <jccosta@MacBook-Air-de-Jose.local>
2023-02-16 17:02:55 +0000

    creating a fetch request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
+import HotelSearch from './components/HotelSearch';
 
 function App() {
   return (
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -5,22 +5,33 @@ import React, { useState } from "react";
 function SearchBar() {
   const [searchInput, setSearchInput] = useState("");
 
+const handleSubmit = async (event) => {
+  const key = event.keyCode
+
+  if(key===13) {
+    console.log(searchInput)
+  }
+  // await fetch
+} 
+
   const handleChange = (event) => {
     event.preventDefault();
+
     setSearchInput(event.target.value);
   };
 
-  if (searchInput.length > 0) {
-    console.log(searchInput);
-  } else {
-    console.log("Please enter a value!");
-  }
+  // if (searchInput.length > 0) {
+  //   console.log(searchInput);
+  // } else {
+  //   console.log("Please enter a value!");
+  // }
 
   return (
     <div className="search-wrapper">
       <input
         type="search"
         placeholder="Search"
+        onKeyDown={handleSubmit}
         onChange={handleChange}
         value={searchInput}
       />
