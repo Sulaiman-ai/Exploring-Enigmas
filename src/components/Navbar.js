a8a040625ed56084344f68d8c24380be4fc09d14
zaitken <zaraaitken18@gmail.com>
2023-02-17 15:24:11 +0000

    adds home component and itinerary component and creates routes for pages using react-router-dom

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
+import {Link} from 'react-router-dom';
+
 function Navbar() {
     return (
         <div className="navbar">
             <a className="nav-logo">Exploring Enigmas</a>
             <nav className="nav-links">
-                <a href="#">My Itinerary</a>
-                <a href="#">Contact</a>
+                <Link to="/">Home</Link>
+                <Link to="/itinerary">Itinerary</Link>
             </nav>
         </div>
     )
---
+++ src/components/Pages/ItineraryPage/Itinerary.js
@@ -0,0 +1,12 @@
+import Navbar from '../../Navbar';
+import Footer from '../../Footer';
+
+function Itinerary() {
+    return(
+        <>
+            <h1>My Itinerary</h1>
+        </>
+    )
+}
+
+export default Itinerary;
