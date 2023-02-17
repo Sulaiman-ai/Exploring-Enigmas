1742ed43f1fe833864d933313b69d7ebae7cbfa2
Sulaiman Amjad <sulaiman.amjad@gmail.com>
2023-02-17 10:41:13 +0000

    nomatim & opentripmap working, apis combined to search for places of interest in a region

--- src/apis/nominatim.js
+++ src/apis/nominatim.js
@@ -1,13 +1,29 @@
+import fetch from 'node-fetch';
+
 const NOMINATIM_BASE_URL = "https://nominatim.openstreetmap.org/";
-const url = "https://nominatim.openstreetmap.org/?addressdetails=1&extratags=1&q=bakery+in+berlin+wedding&format=json&limit=1";
+// const url = "https://nominatim.openstreetmap.org/?addressdetails=1&extratags=1&q=bakery+in+berlin+wedding&format=json&limit=1";
 const params = {
-    q: '',
+    city: '',
     format: 'json',
     addressdetails: 1,
 }
 
-async function searchNominatim(baseURL, params){
-    await fetch(url).then((response)=>console.log(response))
+params.city = 'london';
+
+export function formatNOMURL(baseURL, params){
+    return `${baseURL}search?city=${params.city}&format=${params.format}&addressdetails=1&extratags=1`;
 }
 
-searchNominatim()
+export async function searchNominatim(baseURL, params){
+    const url = formatNOMURL(baseURL, params);
+    const response = await fetch(url);
+    const data = await response.json();
+    const cities = data.filter(place => place.type=="city")
+    // console.log(cities);
+    return cities;
+}
+
+searchNominatim(NOMINATIM_BASE_URL, params);
+console.log("hello")
+
+// export default {formatNOMURL, searchNominatim};
