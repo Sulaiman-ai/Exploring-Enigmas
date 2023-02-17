89088fc04b1964f34a3a9645cc130141fdfc6b4b
zaitken <zaraaitken18@gmail.com>
2023-02-17 15:52:07 +0000

    removes BrowserRouter to fix bug with router conflicts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import {Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar'
 import Home from './components/Pages/Home/Home';
+import Footer from './components/Footer';
 import Itinerary from './components/Pages/ItineraryPage/Itinerary';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/itinerary" element={<Itinerary />} />
       </Routes>
+      <Footer />
     </>
   );
 }
--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,13 +1,9 @@
-import Navbar from '../../Navbar';
 import Hero from '../../Hero';
-import Footer from '../../Footer';
 
 function Home() {
     return (
         <>
-            <Navbar />
             <Hero />
-            <Footer />
         </>
     )
 }
