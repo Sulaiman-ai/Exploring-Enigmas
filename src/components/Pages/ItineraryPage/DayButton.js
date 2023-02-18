c0d9d606da61eeebb01b01ec04e08f5b0fd9a0bf
zaitken <zaraaitken18@gmail.com>
2023-02-18 13:16:22 +0000

    adds basic HTML layout and CSS styling for basic MVP layout to Itinerary page

---
+++ src/components/Pages/ItineraryPage/DayButton.js
@@ -0,0 +1,17 @@
+function DayButton() {
+    return (
+        <>
+            <div className="day-btns">
+                <button className="day-btn">Day 1</button>
+                <button className="day-btn">Day 2</button>
+                <button className="day-btn">Day 3</button>
+                <button className="day-btn">Day 4</button>
+                <button className="day-btn">Day 5</button>
+                <button className="day-btn">Day 6</button>
+                <button className="day-btn">Day 7</button>
+            </div>
+        </>
+    )
+}
+
+export default DayButton;
