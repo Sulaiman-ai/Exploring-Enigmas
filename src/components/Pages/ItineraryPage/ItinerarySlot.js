49b66a28655fcab527592a91b766d090ceb50997
Sulaiman Amjad <sulaiman.amjad@gmail.com>
2023-02-22 14:15:48 +0000

    dried the day1 component with .map, set up localstorage hook, created a component for time slots

---
+++ src/components/Pages/ItineraryPage/ItinerarySlot.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import useLocalStorage from "./LocalStorageHook";
+
+function TimeSlot({index:key}){
+    console.log('key',key)
+    const [storage, setStorage] = useLocalStorage(key, '');
+    const [text, setText] = useState(storage);
+
+    const handleChange = (event) =>{
+        setText(event.target.value);
+    }
+
+    const saveChange = (event) => {
+        setStorage(text);
+    }
+
+    return (
+        <div key={`row-${key}`} className="planner-row">
+            <div className="time">{`${key}`}</div>
+            <textarea key={`${key}`} value={text} className="text-box" onChange={handleChange}></textarea>
+            <button className="save-btn" onClick={saveChange}>Save</button>
+        </div>
+)}
+
+export default TimeSlot;
