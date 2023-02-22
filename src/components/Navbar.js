74f33457343d2e9e98823a9a58d23055ce247fce
zaitken <zaraaitken18@gmail.com>
2023-02-22 16:32:28 +0000

    adds contact page with contact form and web3 forms to receive messages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,7 @@ function Navbar() {
             <nav className="nav-links">
                 <Link to="/">Home</Link>
                 <Link to="/day-one">Itinerary</Link>
+                <Link to="/contact">Contact</Link>
             </nav>
         </div>
     )
